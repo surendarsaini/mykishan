0d87043575d39f11361635fa3d6ff32dc14a5bf2
surendarsain <@surendarsaini>
2026-06-03 00:56:14 +0530

    add parts

---
+++ signup.js
@@ -0,0 +1,25 @@
+
+
+document.getElementById("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    alert("Button Clicked");
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+    
+
+    const user = {
+        name,
+        email,
+        pass
+    
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    console.log(user);
+
+    alert("Signup Successful");
+});
