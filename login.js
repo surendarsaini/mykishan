10099b22e5dbad825d60a4a4891a5da5489015d7
surendarsain <@surendarsaini>
2026-06-03 00:52:38 +0530

    Added login and signup functionality

---
+++ login.js
@@ -0,0 +1,21 @@
+document.getElementById("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("pass");
+
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  console.log("Stored User:", user);
+  console.log("Entered Email:", email);
+  console.log("Entered Password:", pass);
+
+  if(user && 
+    user.email === email ) 
+   {
+    alert("Login Successful");
+     window.location.href = "index.html";
+  } else {
+    alert("Invalid Email");
+  }
+});
